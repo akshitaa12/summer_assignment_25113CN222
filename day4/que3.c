d68eee690b8d49ec3e06ba8e8503ecc5887c6cdc
akshitaa12 <singhakshitaa12@gmail.com>
2026-06-04 13:13:27 +0530

    day3 assignment

---
+++ day4/que3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int n,temp,rem,sum=0;
+    printf("enter a number: ");
+    scanf("%d",&n);
+    temp=n;
+    while(temp!=0){
+        rem = temp % 10;
+        sum =sum + rem * rem * rem;
+      temp = temp/10;
+    }
+    if (sum==n)
+    printf("armstrong number");
+    else 
+    printf("not an armstrong number");
+    return 0;
+}
