2e00ceb51ababc6526f2fd52048adf73e37fb672
akshitaa12 <singhakshitaa12@gmail.com>
2026-06-15 20:52:08 +0530

    day11 assignment

---
+++ day11/que4.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int factorial(int n){
+    int i,fact=1;
+    for(i=1;i<=n;i++)
+    {
+        fact = fact*i;
+    }
+    return fact;
+}
+int main(){
+    int num;
+    printf("enter a number: ");
+   scanf("%d",&num);
+   printf("factorial = %d",factorial(num));
+   return 0;
+}
