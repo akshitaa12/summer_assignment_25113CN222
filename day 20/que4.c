ecd92e17d4073edf636e26d6361ab2e1e21af35a
akshitaa12 <singhakshitaa12@gmail.com>
2026-06-27 10:36:52 +0530

    day20 assignment

---
+++ day 20/que4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int a[10][10], r, c, i, j, sum;
+
+    printf("Enter rows and columns: ");
+    scanf("%d %d", &r, &c);
+
+    printf("enter matrix:\n");
+    for (i = 0; i < r; i++)
+        for (j = 0; j < c ; j++)
+            scanf("%d", &a[i][j]);
+
+    for (j = 0; j < c; j++)
+    {
+        sum = 0;
+        for (i = 0; i < r ; i++)
+            sum = sum + a[i][j];
+
+        printf("sum of columns %d = %d\n", j + 1, sum);
+    }
+    return 0;
+}
